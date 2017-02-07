d8b76c0f3d5e1b9dd3397915cab40aaae859d45c
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-07 13:09:00 +0200

    set the viewholder for list item

--- app/src/main/java/ws/tilda/anastasia/stormy/adapters/DayAdapter.java
+++ app/src/main/java/ws/tilda/anastasia/stormy/adapters/DayAdapter.java
@@ -1,5 +1,68 @@
 package ws.tilda.anastasia.stormy.adapters;
 
-public class DayAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.zip.Inflater;
+
+import ws.tilda.anastasia.stormy.R;
+import ws.tilda.anastasia.stormy.weather.Day;
+
+public class DayAdapter extends BaseAdapter{
+    private Context mContext;
+    private Day[] mDays;
+
+    public DayAdapter(Context context, Day[] days) {
+        mContext = context;
+        mDays = days;
+    }
+
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0; //we aren't gonna use it. Tag items for easy reference.
+    }
+
+
+    //Where mapping happens
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if(convertView == null) {
+            //brand new
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView; //public by default
+        TextView temperatureLabel;
+        TextView dayLabel;
+
+    }
 }
